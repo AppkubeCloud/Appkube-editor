d714c39dafc7082073cbb636448039062aaaaa44
Sagar <sagardevelops15@gmail.com>
2024-03-13 15:58:18 +0530

    added Error messages on fetch or data not compatible with panel

--- public/app/plugins/panel/AppkubeTablePanel/AppkubeTablePanel.tsx
+++ public/app/plugins/panel/AppkubeTablePanel/AppkubeTablePanel.tsx
@@ -8,59 +8,66 @@ class AppkubeTablePanel extends PureComponent<PanelProps> {
   renderTable = (series: any) => {
     const data = JSON.parse(series[0].meta.custom.data);
     const tableBodyJSX: any[] = [];
-    data.forEach((tableRow: any) => {
-      tableBodyJSX.push(
-      <tr>
-        <td>
-          <div className="table-details">{tableRow.Timestamp}</div>
-        </td>
-        <td>
-          <div className="table-details">{tableRow.Alert}</div>
-        </td>
-        <td>
-          <div className="table-details">{tableRow.Description}</div>
-        </td>
-      </tr>
-      )
-    })
-    return (
-      <div className="notifications-table-panel">
-        <div className="notifications-panel-heading">
-          <span className="d-block title">{this.props.options.panelTitle}</span>
-          <button>Filter</button>
+    if(data.length) {
+      data.forEach((tableRow: any) => {
+        tableBodyJSX.push(
+        <tr>
+          <td>
+            <div className="table-details">{tableRow.Timestamp}</div>
+          </td>
+          <td>
+            <div className="table-details">{tableRow.Alert}</div>
+          </td>
+          <td>
+            <div className="table-details">{tableRow.Description}</div>
+          </td>
+        </tr>
+        )
+      })
+      return (
+        <div className="notifications-table-panel">
+          <div className="notifications-panel-heading">
+            <span className="d-block title">{this.props.options.panelTitle}</span>
+            <button>Filter</button>
+          </div>
+          <p className="subtitle">{this.props.options.panelDescription}</p>
+          <div className="common-table">
+            <table cellPadding={0} cellSpacing={0}>
+              <thead>
+                <tr>
+                  <th>
+                    <div className="table-header">Timestamp</div>
+                  </th>
+                  <th>
+                    <div className="table-header">Alert</div>
+                  </th>
+                  <th>
+                    <div className="table-header">Description</div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {tableBodyJSX}
+              </tbody>
+            </table>
+          </div>
         </div>
-        <p className="subtitle">{this.props.options.panelDescription}</p>
-        <div className="common-table">
-          <table cellPadding={0} cellSpacing={0}>
-            <thead>
-              <tr>
-                <th>
-                  <div className="table-header">Timestamp</div>
-                </th>
-                <th>
-                  <div className="table-header">Alert</div>
-                </th>
-                <th>
-                  <div className="table-header">Description</div>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {tableBodyJSX}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    );
+      );
+    } else {
+      return <div style={{height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center'}}>There is some error!</div>
+    }
+   
   }
   
   render() {
     const { data } = this.props;
-    if (data && data.series && data.series[0].meta?.custom?.data) {
+    if(data && data.error) {
+      return <div style={{height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center'}}>{data?.error?.message}</div>;
+    } else if (data && data.series && data.series[0].meta?.custom?.data) {
       const { series } = data;
-      return <div className="notifications-table-panel">{this.renderTable(series)}</div>;
+      return this.renderTable(series);
     } else {
-      return <div>No data available</div>;
+      return <div style={{height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center'}}>No data available</div>;
     }
   }
 }
