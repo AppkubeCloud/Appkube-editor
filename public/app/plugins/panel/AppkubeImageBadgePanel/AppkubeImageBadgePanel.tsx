63179e623bb639e37f502902523974f8f603b91f
Satishpal93 <satishweb93@gmail.com>
2024-03-13 16:33:00 +0530

    Update: CSS changes in AppkubeImageBadgePanel

--- public/app/plugins/panel/AppkubeImageBadgePanel/AppkubeImageBadgePanel.tsx
+++ public/app/plugins/panel/AppkubeImageBadgePanel/AppkubeImageBadgePanel.tsx
@@ -8,15 +8,14 @@ class AppkubeImageBadgePanel extends PureComponent<PanelProps> {
   render() {
     return (
       <div className="image-badge-panel">
-        <div className="badge-circle">
-          <img
-            src={this.props.options.badgeImage}
-            alt=""
-          />
-        </div>
-        <div className="badge-contents">
-          <span title={this.props.options.badgeTitle}>{this.props.options.badgeTitle}</span>
-          <strong>{`540,549`}</strong>
+        <div className="badge-box">
+          <div className="badge-circle">
+            <img src={this.props.options.badgeImage} alt="" />
+          </div>
+          <div className="badge-contents">
+            <span title={this.props.options.badgeTitle}>{this.props.options.badgeTitle}</span>
+            <strong>{`540,549`}</strong>
+          </div>
         </div>
       </div>
     );
