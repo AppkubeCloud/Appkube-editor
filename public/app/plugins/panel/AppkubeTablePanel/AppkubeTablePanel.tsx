d3503527d783b36e31fe6758154cfd39065fd6e7
Jasmin Makasana <jasminmakasana1991@gmail.com>
2024-03-14 13:05:06 +0530

    Table panel updated for dynamic data.

--- public/app/plugins/panel/AppkubeTablePanel/AppkubeTablePanel.tsx
+++ public/app/plugins/panel/AppkubeTablePanel/AppkubeTablePanel.tsx
@@ -8,22 +8,29 @@ class AppkubeTablePanel extends PureComponent<PanelProps> {
   renderTable = (series: any) => {
     const data = JSON.parse(series[0].meta.custom.data);
     const tableBodyJSX: any[] = [];
-    if(data.length) {
+    if (data.length > 0) {
+      const keys = Object.keys(data[0]);
+      const headJSX = keys.map((key) => {
+        return (
+          <th>
+            <div className="table-header">{key}</div>
+          </th>
+        );
+      });
       data.forEach((tableRow: any) => {
+        const rows = keys.map((key) => {
+          return (
+            <td>
+              <div className="table-details">{tableRow[key]}</div>
+            </td>
+          );
+        });
         tableBodyJSX.push(
-        <tr>
-          <td>
-            <div className="table-details">{tableRow.Timestamp}</div>
-          </td>
-          <td>
-            <div className="table-details">{tableRow.Alert}</div>
-          </td>
-          <td>
-            <div className="table-details">{tableRow.Description}</div>
-          </td>
-        </tr>
+          <tr>
+            {rows}
+          </tr>
         )
-      })
+      });
       return (
         <div className="notifications-table-panel">
           <div className="notifications-panel-heading">
@@ -35,15 +42,7 @@ class AppkubeTablePanel extends PureComponent<PanelProps> {
             <table cellPadding={0} cellSpacing={0}>
               <thead>
                 <tr>
-                  <th>
-                    <div className="table-header">Timestamp</div>
-                  </th>
-                  <th>
-                    <div className="table-header">Alert</div>
-                  </th>
-                  <th>
-                    <div className="table-header">Description</div>
-                  </th>
+                  {headJSX}
                 </tr>
               </thead>
               <tbody>
@@ -54,20 +53,20 @@ class AppkubeTablePanel extends PureComponent<PanelProps> {
         </div>
       );
     } else {
-      return <div style={{height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center'}}>There is some error!</div>
+      return <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>There is some error!</div>
     }
-   
+
   }
-  
+
   render() {
     const { data } = this.props;
-    if(data && data.error) {
-      return <div style={{height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center'}}>{data?.error?.message}</div>;
+    if (data && data.error) {
+      return <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>{data?.error?.message}</div>;
     } else if (data && data.series && data.series[0].meta?.custom?.data) {
       const { series } = data;
       return this.renderTable(series);
     } else {
-      return <div style={{height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center'}}>No data available</div>;
+      return <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>No data available</div>;
     }
   }
 }
