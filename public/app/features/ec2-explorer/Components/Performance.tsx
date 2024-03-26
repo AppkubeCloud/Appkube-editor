4d4252e5827924daa436fa51f30a1653d86e38fb
Satishpal93 <satishweb93@gmail.com>
2024-03-26 12:04:09 +0530

    Update: Remove resize and panel title in explorers panels

--- public/app/features/ec2-explorer/Components/Performance.tsx
+++ public/app/features/ec2-explorer/Components/Performance.tsx
@@ -3,18 +3,17 @@ import React, { Component } from 'react';
 import CustomDashboardLoader from 'app/features/custom-dashboard-loader';
 
 class Performance extends Component {
-  constructor(props: Record<string,string>) {
+  constructor(props: Record<string, string>) {
     super(props);
     this.state = {};
   }
 
   render() {
-    return <div>
-      <CustomDashboardLoader
-        uid={"6haWC40Sz"}
-        slug="new-dashboard"
-      />
-    </div>;
+    return (
+      <div className="dashboard-panels-container">
+        <CustomDashboardLoader uid={'O0KpnF1Sk'} slug="new-dashboard" />
+      </div>
+    );
   }
 }
 
